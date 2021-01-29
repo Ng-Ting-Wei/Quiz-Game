1bad257e7d512288de0c7bb05ba448f037d7c19a
Xenousss <64124324+Xenousss@users.noreply.github.com>
2021-01-29 12:04:36 +0800

    Updating the door questions
    Managed to implement a few questions on the door to print out. However, needs some fine tuning.

--- Quiz Game/Assets/Script/DoorQuestion.cs
+++ Quiz Game/Assets/Script/DoorQuestion.cs
@@ -9,8 +9,10 @@ public class DoorQuestion : MonoBehaviour
     private float openDistance = 3f;
     private float openSpeed = 2.0f;
     public Transform doorBody;
-    bool open = false;
+    private bool open = false;
+    private bool istriggered = false;
     Vector3 defaultDoorPosition;
+    private int questionNumber;
 
     private void Start()
     {
@@ -41,18 +43,56 @@ public class DoorQuestion : MonoBehaviour
         }
     }
 
+    private void questionRandom()
+    {
+        if (istriggered == true)
+        {
+            questionNumber = Random.Range(0, 6);
+            switch (questionNumber)
+            {
+                case 5:
+                    print("Which BC did Rome's implement a national army?");
+                    //550 BC
+                    break;
+                case 4:
+                    print("During the early days of the roman army, which king implemented a Rome's national army?");
+                    //king Servius Tullius
+                    break;
+                case 3:
+                    print("During the early days of the roman army, what were the roman light cavalry called?");
+                    //Equites Celeres
+                    break;
+                case 2:
+                    print("Which Bc did Rome conquered gaul");
+                    //121 BC
+                    break;
+                case 1:
+                    print("What was rome's form of government before becoming an empire");
+                    //Republic
+                    break;
+                case 0:
+                    print("Which BC did Rome conquered the seas in the Mediterranean");
+                    //264 B.C. to 146 B.C.
+                    break;
+            }
+        }
+    }
+
     private void OnTriggerEnter(Collider enter)
     {
         if (enter.CompareTag("Player"))
         {
+            istriggered = true;
             open = true;
         }
+        questionRandom();
     }
 
     private void OnTriggerExit(Collider exit)
     {
         if (exit.CompareTag("Player"))
         {
+            istriggered = false;
             open = false;
         }
     }
