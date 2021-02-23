fa7ee905cca580e6de21191c6456fdde22a33577
CheongYanSeng <64124487+CheongYanSeng@users.noreply.github.com>
2021-02-23 14:57:04 +0800

    Highscore issue

---
+++ Quiz Game/Assets/Script/Highscore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Highscore : MonoBehaviour
+{
+    public Text HighscoreText;
+    public Text ScoreHighscoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HighscoreText.text = PlayerPrefs.GetFloat("hc").ToString("0");
+        ScoreHighscoreText.text = PlayerPrefs.GetInt("ScoreHighscore", 0).ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
